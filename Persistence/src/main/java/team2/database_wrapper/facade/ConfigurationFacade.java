fa9759a65c507719b04bca8c65ce30137212daac
Helmut Rhomberg <hrh5775@students.fhv.at>
2017-10-25 11:04:34 +0200

    add more facades

---
+++ Persistence/src/main/java/team2/database_wrapper/facade/ConfigurationFacade.java
@@ -0,0 +1,109 @@
+package team2.database_wrapper.facade;
+
+import org.modelmapper.ModelMapper;
+import team2.database_wrapper.entities.ConfigurationEntity;
+import team2.database_wrapper.helper.MapperHelper;
+import team2.database_wrapper.helper.StoreHelper;
+import team2.domain.entities.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ConfigurationFacade extends BaseDatabaseFacade<Configuration> {
+    private List<Configuration> listType = new LinkedList<>();
+
+    public ConfigurationFacade() {
+        super();
+    }
+
+    public ConfigurationFacade(EntityManager session) {
+        super(session);
+    }
+
+    @Override
+    public Configuration getById(int id) {
+        EntityManager session = getCurrentSession();
+        Query query = session.createQuery("from ConfigurationEntity where id = :id");
+        query.setParameter("id", id);
+        query.setMaxResults(1);
+        List<ConfigurationEntity> entities = query.getResultList();
+
+        if (entities.size() > 0) {
+            ConfigurationEntity entity = entities.get(0);
+
+            ModelMapper mapper = MapperHelper.getMapper();
+            return mapper.map(entity, Configuration.class);
+        }
+
+        return null;
+    }
+
+    public Configuration getByKey(String key) {
+        EntityManager session = getCurrentSession();
+        Query query = session.createQuery("from ConfigurationEntity where identifier = :key");
+        query.setParameter("key", key);
+        query.setMaxResults(1);
+        List<ConfigurationEntity> entities = query.getResultList();
+
+        if (entities.size() > 0) {
+            ConfigurationEntity entity = entities.get(0);
+
+            ModelMapper mapper = MapperHelper.getMapper();
+            return mapper.map(entity, Configuration.class);
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Configuration> getList() {
+        EntityManager session = getCurrentSession();
+        Query query = session.createQuery("from ConfigurationEntity");
+        List<ConfigurationEntity> entities = query.getResultList();
+        ModelMapper mapper = MapperHelper.getMapper();
+
+        return mapper.map(entities, listType.getClass());
+    }
+
+    @Override
+    public int add(Configuration value) {
+        EntityManager session = getCurrentSession();
+        session.getTransaction().begin();
+
+        ModelMapper mapper = MapperHelper.getMapper();
+        ConfigurationEntity entity = mapper.map(value, ConfigurationEntity.class);
+
+        session.persist(entity);
+        session.flush();
+        StoreHelper.storeEntities(session);
+
+        return entity.getId();
+    }
+
+    @Override
+    public int update(Configuration value) {
+        EntityManager session = getCurrentSession();
+        session.getTransaction().begin();
+
+        ModelMapper mapper = MapperHelper.getMapper();
+        ConfigurationEntity entity = mapper.map(value, ConfigurationEntity.class);
+
+        session.merge(entity);
+        StoreHelper.storeEntities(session);
+
+        return entity.getId();
+    }
+
+    @Override
+    public boolean delete(int id) {
+        EntityManager session = getCurrentSession();
+        session.getTransaction().begin();
+        Query query = session.createQuery("delete ConfigurationEntity where id = :id");
+        query.setParameter("id", id);
+        query.executeUpdate();
+
+        return StoreHelper.storeEntities(session);
+    }
+}
---
+++ Persistence/src/test/java/CreatorTypeFacadeTest.java
@@ -0,0 +1,54 @@
+import org.junit.Assert;
+import org.junit.Test;
+import team2.database_wrapper.facade.CreatorTypeFacade;
+import team2.domain.entities.CreatorType;
+
+import javax.naming.NamingException;
+import java.util.List;
+
+public class CreatorTypeFacadeTest {
+    @Test
+    public void testAll() throws NamingException {
+        int id = testAdd();
+        testById(id);
+        testGetList();
+        testUpdate(id);
+        testDelete(id);
+    }
+
+    private CreatorType testById(int id) {
+        CreatorTypeFacade facade = new CreatorTypeFacade();
+        CreatorType result = facade.getById(id);
+        Assert.assertNotNull(result);
+
+        return result;
+    }
+
+    private void testGetList() {
+        CreatorTypeFacade facade = new CreatorTypeFacade();
+        List<CreatorType> list = facade.getList();
+        Assert.assertTrue(list.size() > 0);
+    }
+
+    private int testAdd() {
+        CreatorTypeFacade facade = new CreatorTypeFacade();
+        CreatorType value = new CreatorType();
+        value.setTypeName("Creator Type Test");
+        int id = facade.add(value);
+        Assert.assertTrue(id > 0);
+
+        return id;
+    }
+
+    private void testUpdate(int id) {
+        CreatorTypeFacade facade = new CreatorTypeFacade();
+        CreatorType value = facade.getById(id);
+        value.setTypeName("Creator Type Test 2");
+        Assert.assertTrue(facade.update(value) > 0);
+    }
+
+    private void testDelete(int id) {
+        CreatorTypeFacade facade = new CreatorTypeFacade();
+        Assert.assertTrue(facade.delete(id));
+    }
+}
