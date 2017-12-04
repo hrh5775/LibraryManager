2bcb89219f6979feb0da713be6ca09e4768b2014
Feuerstein, Julian <Feuerstein.julian@gmail.com>
2017-12-04 21:19:01 +0100

    Fernleihe Button Logik
    TODO: Klasse aus dem anderen Branch fehlt noch
    (code momentan auskommentiert)
    siehe: MessageRemoteObject --> sendMessageForInterLibraryLoan(String message)

--- Client/src/main/java/at/team2/client/pages/viewreadremoteloan/ViewReadRemoteLoan.java
+++ Client/src/main/java/at/team2/client/pages/viewreadremoteloan/ViewReadRemoteLoan.java
@@ -1,10 +1,24 @@
 package at.team2.client.pages.viewreadremoteloan;
 
 import javax.lang.model.type.NullType;
+
+import at.team2.client.helper.RmiHelper;
 import at.team2.client.pages.BasePage;
+import at.team2.common.interfaces.MainRemoteObjectInf;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 public class ViewReadRemoteLoan extends BasePage<Void,NullType,NullType,NullType> {
+    @FXML
+    private TextField _messageTextfield;
+    @FXML
+    private Button _sendButton;
+
     @Override
     public void initialize() {
         Parent parent = loadView(ViewReadRemoteLoan.class.getResource("view_read_remote_loan.fxml"));
@@ -29,5 +43,17 @@ public class ViewReadRemoteLoan extends BasePage<Void,NullType,NullType,NullType
 
     @Override
     public void dispose() {
+    }
+
+    @FXML
+    private void send() {
+        try {
+            MainRemoteObjectInf remoteObject = RmiHelper.getSession();
+            remoteObject.getMessageRemoteObject().sendMessageForInterLibraryLoan(_messageTextfield.getText());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
     }
 }
