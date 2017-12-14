1036c33a035495e86a42764bb388e7cefaefc635
Helmut Rhomberg <hrh5775@students.fhv.at>
2017-12-14 10:00:11 +0100

    improve media search

---
+++ Web-Application/src/main/java/at/team2/webapplication/beans/SearchMedia.java
@@ -0,0 +1,57 @@
+package at.team2.webapplication.beans;
+
+import at.team2.common.dto.small.MediaSmallDto;
+import at.team2.common.interfaces.ejb.BookRemote;
+import at.team2.common.interfaces.ejb.DvdRemote;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+@ManagedBean
+@SessionScoped
+public class SearchMedia implements Serializable {
+    @EJB
+    private DvdRemote _dvdRemote;
+    @EJB
+    private BookRemote _bookRemote;
+    private List<MediaSmallDto> _mediaList;
+    private String _searchValue;
+
+    public SearchMedia() {
+        _mediaList = new LinkedList<>();
+        _searchValue = "";
+    }
+
+    public List<MediaSmallDto> getMediaList() {
+        return _mediaList;
+    }
+
+    public String getSearchValue() {
+        return _searchValue;
+    }
+
+    public void setSearchValue(String searchValue) {
+        _searchValue = searchValue;
+    }
+
+    public boolean getIsSearchButtonDisabled() {
+        return _searchValue == null || _searchValue.length() == 0;
+    }
+
+    public void search() {
+        _mediaList.clear();
+
+        if(!getIsSearchButtonDisabled()) {
+            _mediaList.addAll(_bookRemote.getBookSmallList(getSearchValue()));
+            _mediaList.addAll(_dvdRemote.getDvdSmallList(getSearchValue()));
+        }
+    }
+
+    public void showDetails(MediaSmallDto media) {
+        // @todo:
+    }
+}
